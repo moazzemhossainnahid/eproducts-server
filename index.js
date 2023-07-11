322c85acfdc846602c9ae211e88ef099f6a4c1e4
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-07-12 00:07:16 +0600

    add client url

--- index.js
+++ index.js
@@ -132,25 +132,30 @@ const run = async () => {
         // Sign in to see your own test API key embedded in code samples.
 
 
-        // const YOUR_DOMAIN = 'http://localhost:5173';
 
-        // app.post('/create-checkout-session', async (req, res) => {
-        //     const { price } = req.body;
-        //     const session = await stripe.checkout.sessions.create({
-        //         line_items: [
-        //             {
-        //                 // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        //                 price: 'price_1NSNOUKQApjCPl5WxTaBsOEc',
-        //                 quantity: 1,
-        //             },
-        //         ],
-        //         mode: 'payment',
-        //         success_url: `${YOUR_DOMAIN}?success=true`,
-        //         cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-        //     });
+        app.post('/create-checkout-session', async (req, res) => {
+            const { price } = req.body;
+            const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                        price_data: {
+                            currency: "usd",
+                            product_data: {
+                                name: "Carport"
+                            },
+                            unit_amount: 3700,
+                        },
+                        quantity: 1,
+                    },
+                ],
+                mode: 'payment',
+                success_url: `${process.env.CLIENT_URL}/checkout-success`,
+                cancel_url: `${process.env.CLIENT_URL}/cart`,
+            });
 
-        //     res.redirect(303, session.url);
-        // });
+            res.redirect(303, session.url);
+        });
 
 
 
